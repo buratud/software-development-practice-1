8c304e8d4e7ddc985414883940bb9a7658c91539
KeneticKills <109983145+KeneticKills@users.noreply.github.com>
2023-08-18 18:22:35 +0700

    Update Q4.c

--- C Problems/Q4.c
+++ C Problems/Q4.c
@@ -3,25 +3,34 @@
 // str is an array of chars passed as the argument to the function.
 int detect_pattern(const char *str)
 {
+    //Get length of string in str variable
     int length = strlen(str);
+    //This will contain state of pattern
     int state = 0;
+    // Iterate through every character in string
     for (int i = 0; i < length; i++)
     {
+        //If character is 1 then check the state
         if (*str == '1')
         {
+            //If state is 0, Set state to 1
             if (state == 0)
             {
                 state = 1;
             }
+
+            //but, If state is 2, Immediately return 1
             else if (state == 2)
             {
                 return 1;
             }
         }
+        //In otherhand, If character is 0 and state is not 0, Set the state to 2
         else if (*str == '0' && state != 0)
         {
             state = 2;
         }
+        //so, If character is not 1, or character is not 0 and state is 0 at the same time, Reset the state
         else
         {
             state = 0;
