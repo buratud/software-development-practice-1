c3e49f0ceaff1d1012ad17725d62fc926c513d12
KeneticKills <109983145+KeneticKills@users.noreply.github.com>
2023-09-15 16:45:24 +0700

    Update sketch_sep12b.ino
    -add click, double click and long press to digital input
    -change == in string compare to string.compareTo(string)

--- Node-RED/sketch_sep12b/sketch_sep12b.ino
+++ Node-RED/sketch_sep12b/sketch_sep12b.ino
@@ -1,6 +1,6 @@
-#define LED_R 32
-#define LED_G 33
-#define LED_B 25
+#define LED_R_PIN 32
+#define LED_G_PIN 33
+#define LED_B_PIN 25
 #define INPUT_DIGITAL 34
 #define INPUT_ANALOG 35
 
@@ -12,29 +12,33 @@ void handleBlink();
 void handlePwm();
 
 int numLEDs = 3;
-int ledPins[] = { LED_R, LED_G, LED_B };  // Array to store LED pins
+int ledPins[] = { LED_R_PIN, LED_G_PIN, LED_B_PIN };  // Array to store LED pins
 //int blinkSpeeds[] = { 0, 0, 0 };               // Array to store blinking speeds (in milliseconds)
 int blinkSpeed = 0;
 unsigned long long previousMilli = 0;
 //unsigned long previousMillis[] = { 0, 0, 0 };  // Array to store previous times for blinking
 String mode = "";
 int prevButtonState, currentButtonState;
-unsigned int currentPwm, prevPwm;
+int clickTime = 500;      // Countdown before it count as a long press in millisec
+bool pressState = false;  // Keep Pressing State (True when press ; False when release)
+bool doubleClickState = false;    // Keep Double Click State to prevent auto reset (True when press ; False when release)
+unsigned int currentPwm, prevPwm, prevClick;
 
 void setup() {
   Serial.begin(115200);
-  pinMode(LED_R, OUTPUT);
-  pinMode(LED_G, OUTPUT);
-  pinMode(LED_B, OUTPUT);
+  pinMode(LED_R_PIN, OUTPUT);
+  pinMode(LED_G_PIN, OUTPUT);
+  pinMode(LED_B_PIN, OUTPUT);
   pinMode(INPUT_DIGITAL, INPUT_PULLUP);
   pinMode(INPUT_ANALOG, INPUT);
-  digitalWrite(LED_R, HIGH);
-  digitalWrite(LED_G, HIGH);
-  digitalWrite(LED_B, HIGH);
+  digitalWrite(LED_R_PIN, HIGH);
+  digitalWrite(LED_G_PIN, HIGH);
+  digitalWrite(LED_B_PIN, HIGH);
   prevButtonState = digitalRead(INPUT_DIGITAL);
   currentButtonState = prevButtonState;
   prevPwm = analogRead(INPUT_ANALOG);
   currentPwm = prevPwm;
+  prevClick = 0;
   Serial.println("db,0");
   Serial.println("an,0");
   Serial.setTimeout(50);
@@ -45,15 +49,15 @@ void loop() {
   // put your main code here, to run repeatedly:
   if (Serial.available()) {
     mode = Serial.readStringUntil(',');
-    if (mode == "turn") {
+    if (mode.compareTo("turn") == 0) {
       handleTurn();
-    } else if (mode == "blink") {
+    } else if (mode.compareTo("blink") == 0) {
       handleBlink();
-    } else if (mode == "pwm") {
+    } else if (mode.compareTo("pwm") == 0) {
       handlePwm();
     }
   }
-  if (mode == "blink") {
+  if (mode.compareTo("blink") == 0) {
     if (blinkSpeed > 0) {
       if (millis() - previousMilli >= blinkSpeed) {
         // for (int i = 0; i < numLEDs; i++) {
@@ -61,7 +65,7 @@ void loop() {
         //   previousMilli = millis();
         //   digitalWrite(pin, !digitalRead(pin));
         // }
-        int pin = LED_R;
+        int pin = LED_R_PIN;
         previousMilli = millis();
         digitalWrite(pin, !digitalRead(pin));
       }
@@ -71,9 +75,29 @@ void loop() {
   currentPwm = analogRead(INPUT_ANALOG);
   if (currentButtonState != prevButtonState) {
     if (currentButtonState == LOW) {
-      Serial.println("db,1");
+      doubleClickState = false;
+      if(millis() - prevClick < clickTime){
+        doubleClickState = true;
+        //Click after first click less than 0.5 sec
+        Serial.println("db,Double Click");
+      }
+      prevClick = millis();
+      pressState = true;
     } else {
-      Serial.println("db,0");
+      pressState = false;
+      if (millis() - prevClick < clickTime) {
+        prevClick = millis();
+      }
+    }
+  }
+  if (millis() - prevClick >= clickTime) {
+    if(pressState){
+      //If button is press more than 0.5 second
+      Serial.println("db,Long Press");
+    }
+    else if (!doubleClickState) {
+      //If button is not press for 0.5 second after last click
+      Serial.println("db,Click");
     }
   }
   if (currentPwm != prevPwm) {
@@ -87,9 +111,9 @@ void loop() {
 void handleTurn() {
   int pin = Serial.readStringUntil(',').toInt();
   String mode = Serial.readStringUntil('\n');
-  if (mode == "on") {
+  if (mode.compareTo("on") == 0) {
     digitalWrite(pin, LOW);
-  } else if (mode == "off") {
+  } else if (mode.compareTo("off") == 0) {
     digitalWrite(pin, HIGH);
   }
 }
@@ -97,12 +121,12 @@ void handleTurn() {
 void handleBlink() {
   int pin = Serial.readStringUntil(',').toInt();
   String mode = Serial.readStringUntil('\n');
-  if (mode == "off") {
+  if (mode.compareTo("off") == 0) {
     blinkSpeed = 0;
-    digitalWrite(LED_R, HIGH);
-  } else if (mode == "slow") {
+    digitalWrite(LED_R_PIN, HIGH);
+  } else if (mode.compareTo("slow") == 0) {
     blinkSpeed = SLOW_SPEED;
-  } else if (mode == "fast") {
+  } else if (mode.compareTo("fast") == 0) {
     blinkSpeed = FAST_SPEED;
   }
   previousMilli = millis();
